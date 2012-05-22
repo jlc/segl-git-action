e612c9b5b89422a3d714f8b755eb547a8b208c4c
Jeanluc Chasseriau <jeanluc.chasseriau@crossing-tech.com>
2012-05-23 01:39:39 +0200

    Exercise 2 done

--- src/main/scala/Easy.scala
+++ src/main/scala/Easy.scala
@@ -8,3 +8,15 @@ object Ex1 {
     case _         => throw new NoSuchElementException
   }
 }
+
+object Ex2 {
+  def nthBuiltin[A](n: Int, ls: List[A]): A =
+    if (n >= 0) ls(n)
+    else throw new NoSuchElementException
+
+  def nthRecursive[A](n: Int, ls: List[A]): A = (n, ls) match {
+    case (0, h :: _   ) => h
+    case (n, _ :: tail) => nthRecursive(n - 1, tail)
+    case (_, Nil      ) => throw new NoSuchElementException
+  }
+}
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -7,6 +7,11 @@ object HelloWorld {
     val ls1 = List(1,2,3,4)
     println(Ex1.lastBuiltin(ls1) )
     println(Ex1.lastRecursive(ls1))
+
+    println("Ex2:")
+    val ls2 = List(5,6,7,8)
+    println(Ex2.nthBuiltin(2, ls2))
+    println(Ex2.nthRecursive(2, ls2))
   }
 }
 
