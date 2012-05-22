2d71da62cfa5e9e3c532f32f5acc330d77f037be
Jeanluc Chasseriau <jeanluc.chasseriau@crossing-tech.com>
2012-05-23 01:21:31 +0200

    Call exercise from main

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -2,6 +2,11 @@
 object HelloWorld {
   def main(args: Array[String]) {
     println("Hello World... again and again...")
+
+    println("Ex1:")
+    val ls1 = List(1,2,3,4)
+    println(Ex1.lastBuiltin(ls1) )
+    println(Ex1.lastRecursive(ls1))
   }
 }
 
